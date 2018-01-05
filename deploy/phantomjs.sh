532804a4a58d06449bd0bbb3714eca6390e2a520
Hai Tran <minhhaiweb@gmail.com>
2018-01-05 21:37:40 +0700

    Update PhantomJS configuration

--- deploy/phantomjs.sh
+++ deploy/phantomjs.sh
@@ -9,14 +9,14 @@ apt-get update  -y
 apt-get install build-essential chrpath git-core libssl-dev libfontconfig1-dev wget -y
 
 # download phantomjs and decompress
-wget https://phantomjs.googlecode.com/files/phantomjs-1.9.2-linux-x86_64.tar.bz2
-tar xjf phantomjs-1.9.2-linux-x86_64.tar.bz2
+wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2
+tar xjf phantomjs-2.1.1-linux-x86_64.tar.bz2
 
 # install binary
-install -t /usr/local/bin phantomjs-1.9.2-linux-x86_64/bin/phantomjs
+install -t /usr/local/bin phantomjs-2.1.1-linux-x86_64/bin/phantomjs
 
 # cleanup after ourselves
-rm -rf phantomjs-1.9.2-linux-x86_64
-rm phantomjs-1.9.2-linux-x86_64.tar.bz2
+rm -rf phantomjs-2.1.1-linux-x86_64
+rm phantomjs-2.1.1-linux-x86_64.tar.bz2
 
 popd
