cc83698138cba1ea5f99b6aec145b23f8a9af1fa
Hai Tran <minhhaiweb@gmail.com>
2018-01-05 21:24:41 +0700

    Update PhantomJS configuration

--- deploy/phantomjs.sh
+++ deploy/phantomjs.sh
@@ -5,15 +5,15 @@ set -e
 pushd /tmp
 
 # install dependencies
-sudo apt-get update
-sudo apt-get install build-essential chrpath git-core libssl-dev libfontconfig1-dev
+apt-get update
+apt-get install build-essential chrpath git-core libssl-dev libfontconfig1-dev
 
 # download phantomjs and decompress
 wget https://phantomjs.googlecode.com/files/phantomjs-1.9.2-linux-x86_64.tar.bz2
 tar xjf phantomjs-1.9.2-linux-x86_64.tar.bz2
 
 # install binary
-sudo install -t /usr/local/bin phantomjs-1.9.2-linux-x86_64/bin/phantomjs
+install -t /usr/local/bin phantomjs-1.9.2-linux-x86_64/bin/phantomjs
 
 # cleanup after ourselves
 rm -rf phantomjs-1.9.2-linux-x86_64
