96aa9c49793d0d3a8027d31b660dd93699f17106
Hai Tran <minhhaiweb@gmail.com>
2018-01-09 06:22:52 +0700

    Update test script for Demo page

--- tests/acceptance/FirstCest.php
+++ tests/acceptance/FirstCest.php
@@ -15,13 +15,9 @@ class FirstCest
     public function homePageTest(AcceptanceTester $I)
     {
         $title = 'Laravel';
-        $I->amOnPage('/');
-        // $I->expect(sprintf('See the title with value = "%s"', $title));
-            // $I->seeInTitle($title);
-        // $I->expect(sprintf('See element in the main content with value = "%s"', $title));
-            // $I->seeElement(sprintf('//*/div[@class="content"]/div[@class="title m-b-md" and text()="%s"]', $title));
-        // $I->expect('See a link navigate to Laravel documentation address');
-            // $I->seeLink('Documentation','https://laravel.com/docs');
+        $I->expect(sprintf('See the title with value = "%s"', $title));
+        $I->expect(sprintf('See element in the main content with value = "%s"', $title));
+        $I->expect('See a link navigate to Laravel documentation address');
     }
 
     // test demo page
@@ -29,12 +25,8 @@ class FirstCest
     {
         $title = 'list item';
         $mainContent = 'The title is: list item';
-        $I->amOnPage('/demo');
-        // $I->expect(sprintf('See the title with value = %s', $title));
-            // $I->seeInTitle($title);
-        // $I->expect(sprintf('See element in the main content with value = "%s"', $mainContent));
-            // $I->seeElement(sprintf('//*/div[@class="content"]/div[@class="title m-b-md" and text()="%s"]', $mainContent));
-        // $I->expect('See a link navigate to Laravel Github Repo address');
-            // $I->seeLink('GitHub','https://github.com/laravel/laravel');
+        $I->expect(sprintf('See the title with value = %s', $title));
+        $I->expect(sprintf('See element in the main content with value = "%s"', $mainContent));
+        $I->expect('See a link navigate to Laravel Github Repo address');
     }
 }
